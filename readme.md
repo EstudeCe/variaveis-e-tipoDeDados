af0bb7155f444c053ba04d1764ec91197dbdb2fb
Vitor Sousa <v.sousa.cf@gmail.com>
2023-04-07 14:54:23 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+## Variáveis e tipos de dados
+
+O intuito desta primeira aula é abordar os conceitos iniciais sobre variáveis e tipos de dados existentes no JavaScript. E iniciar os estudos de condicionais utilizando operadores lógicos.
+
+**Criar e vincular o script no HTML**
+
+Criei um arquivo index.html, e la no final do body passei a tag script com o atributo src e o caminho do arquivo script
+
+```
+<body>
+    <script src="script.js"></script>
+</body>
+```
---
+++ script.js
@@ -0,0 +1,90 @@
+let cash = 10;
+// var cash2 = 20;
+
+const cash3 = "35";
+
+// Tipo string
+const text = "Essa loja tem um desconto";
+const desconto = "15%";
+
+// Tipo Number
+const number = 0.5 / 12;
+// console.log(100 % 9);
+
+// Tipo Array - lista
+const lista = [10, 20, "string", [2, 3, 4], { cor: "Marrom" }];
+// Voltar aqui para acessar os index
+
+// Tipo Object - Objeto
+// Par chave valor
+const cadeira = {
+  cor: "Preto",
+  preco: 400,
+  lojas: ["Magazine luiza", "Kabum"],
+  script: {
+    linguagem: "Javascript",
+  },
+};
+
+// Desestruturação
+// const cor = cadeira.cor
+const lojas = cadeira.lojas;
+// const preco = cadeira.preco
+// const script = cadeira.script;
+const { cor, preco } = cadeira;
+cadeira.desconto = "15%";
+// console.log(preco, "preco");
+
+const teste = [
+  {
+    nome: "Vitor",
+    nota: 10,
+  },
+  {
+    nome: "Erika",
+    nota: 10,
+  },
+  {
+    nome: "Joaquim",
+    nota: 7,
+  },
+];
+
+const aluno = teste[1];
+const { nota, nome } = aluno;
+// console.log("A nota do " + nome + " é igual a " + nota);
+//Template String
+// console.log(`A nota do ${nome} é igual a ${nota}`);
+
+// Tipo boolean
+const rain = true;
+
+// Truthy - Qualquer coisa
+
+// Falsy
+// 0
+// Undefined
+// null
+// NaN
+
+const desconto2 = Boolean(NaN);
+
+const price = 100;
+const percent = 12;
+const secondPercent = true;
+
+if (percent == 0) {
+  console.log("Não temos desconto para essa cadeira");
+  if (secondPercent) {
+    console.log("Você deu sorte hoje, temos outro desconto!");
+  }
+} else if (percent > 10) {
+  console.log("É maior do que 10");
+} else {
+  console.log(`Temos um desconto de ${percent}%`);
+}
+
+// Armazenar o salário de alguém em uma variável
+// Salário é até 1000 reais - Console.log
+// Salário esta entre 1000 e 2000 - Console.log
+// Salário maior do que 2000 - Console.log
